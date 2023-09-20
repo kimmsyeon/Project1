937cbb0ec659bc08ec629618b3eebca5171e3a9a
Kimsoyeon <sykim557@naver.com>
2023-09-20 15:25:05 +0900

    커밋 1 더하기 기능 구현

---
+++ main.py
@@ -0,0 +1,11 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ## 함수 선언부
+>>> def add_func(n1,n2) :
+...     return n1+n2
+... 
+>>> ## 전역 변수부
+>>> num1,num2,res = 100,200,0
+>>> 
+>>> ## 메인 코드부
+>>> res = add_func(num1,num2)
